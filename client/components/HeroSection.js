4f0ef0ad1d7da5944c0e84519e180490a26c86fa
moore76sc <moore76sc@gmail.com>
2022-01-19 06:15:14 -0800

    corrected all instances of _class_ to _className_

--- client/components/HeroSection.js
+++ client/components/HeroSection.js
@@ -6,7 +6,7 @@ import './styles/HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/Police.mp4' autoPlay loop muted />
+      {/*<video src='../videos/Police.mp4' autoPlay loop muted />*/}
       <h1>FBI Most Wanted</h1>
    
       </div>
