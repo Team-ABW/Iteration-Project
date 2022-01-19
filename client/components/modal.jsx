f90a8b39fe4dc203d64f6d114c9220d1d699e204
moore76sc <moore76sc@gmail.com>
2022-01-19 12:52:08 -0800

    formatting for modal

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -4,14 +4,12 @@ import './styles/modal.css';
 const Modal = ({display, handleModalOpen}) => {
 
 return(
-<div id= "modal">
-
+  <div id= "modal">
     <section id="modal-main">
-    
-        {display}   
-        <button id="closeButton" type="button" onClick={handleModalOpen}> Close </button>
-      </section>
-    </div>
+      {display}   
+      <button id="closeButton" type="button" onClick={handleModalOpen}> Close </button>
+    </section>
+  </div>
 )
 };
 export default Modal;
