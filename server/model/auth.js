cf76ed92b0c55a42173bede704ebb29fa4751c4b
moore76sc <moore76sc@gmail.com>
2022-01-19 13:52:36 -0800

    completed model for auth with mongoose

---
+++ server/model/auth.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const MONGO_URI = 'mongodb+srv://moore76sc:Tomoe288476!@cluster0.gqcrs.mongodb.net/Sweat_Tracker?retryWrites=true&w=majority'
+
+mongoose.connect(MONGO_URI, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+dbName: 'Most-Wanted-Auth'
+})
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch(err => console.log(err));
+
+  const Schema = mongoose.Schema;
+
+const sessionSchema = new Schema({
+  cookieId: { type: String, required: true, unique: true },
+  createdAt: { type: Date, expires: 30, default: Date.now }
+});
+
+const Session = mongoose.model('Session', sessionSchema);
+
+const SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcryptjs');
+
+const userSchema = new Schema({
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true}
+});
+userSchema.pre('save', function(next) {
+  bcrypt.hash(this.password, SALT_WORK_FACTOR, (err, hash) => {
+  if (err) return next(err)
+  this.password = hash;
+    return next();
+  })
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = {
+  Session,
+  User
+  };
